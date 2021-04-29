44841ab20d50a670ee434dd9f9def58edcccff1a
jkelly101 <jenLkelly@gmail.com>
2021-04-28 22:57:59 -0400

    added constructor and API call to App.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,9 +1,53 @@
-import React, {Component} from 'react';
-import './App.css';
-import Title from './src/components/Title';
+import React, { Component } from "react";
+import "./App.css";
+import Title from "./src/components/Title";
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      loading: false,
+    };
+  }
+
+  componenetDidMount() {
+    fetch("https://randomuser.me/api/")
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          items: response.results,
+          loading: true,
+        });
+      });
+  }
+
+  // state = {
+  //   employees: [...employees],
+  // };
+
+  // handleSort = (key, asc) => {
+  //   let employeesSorted = [...this.state.employees];
+
+  //   // add additional sort options
+  //   employeesSorted.sort((a, b) => {
+  //     return a[key] > b[key] ? asc * 1 : asc - 1;
+  //   });
+
+  //   this.setState({ employees: employeesSorted });
+  // };
+
+  render() {
+    var { items, loading } = this.state;
+
+    if (!loading) {
+      return <div>Loading...</div>;
+    } else {
+      return;
+      // what will show on page
+      <Title>Employee Directory</Title>;
+    }
+  }
 }
 
 export default App;
